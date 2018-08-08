264c47b1d8b62ba094ec2dba98a28da0f1fbabf0
Maude <whippou@gmail.com>
2018-08-08 12:09:43 -0400

    Syntax error

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -23,7 +23,7 @@ function dictToJava(args){
 		if (args.hasOwnProperty(key)) {
 			//var flag_name = key.slice(getParamPos + dict[launchfile].length);
 			var flag_name = key;
-			if (flag_name.chatAt(0) == "/"){
+			if (flag_name.charAt(0) == "/"){
 				flag_name = flag_name.substr(1);
 			}
 			var value = args[key];
