4b56d6bbd7ff1e4cb1d04aa24725ef66288cacfd
Maude <whippou@gmail.com>
2018-08-08 12:24:58 -0400

    JQuery didnt work, trying other method

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -41,7 +41,7 @@ function dictToJava(args){
 
 function findCategory(arg){
 	for (var key in dict){
-		if (arg.includes(dict[key]) || $.inArray(arg, key) != -1){
+		if (arg.includes(dict[key]) || key.indexOf(arg) > 1 ){
 			return key;
 		}
 	}
