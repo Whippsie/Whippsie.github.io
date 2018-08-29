c10511c3172296c4ae4f0f7716e52a29ed2d58ca
Maude <whippou@gmail.com>
2018-08-28 21:26:36 -0400

    Fixed GMF generation in pathing on PC, Jar working!

--- mymodule.js
+++ mymodule.js
@@ -14,7 +14,7 @@ var exec = require('child_process').exec;
 function executeJava(location,file) {
 	  //child_process.exec(command[, options][, callback])
       //exec('java -jar E:/NeonWorks/canard.jar '+ "input/demo.launch",
-	  exec('java -jar '+ location + ' ' + file + '.launch ' + file + '_GMF.canard',
+	  exec('java -jar '+ location + ' "'+location+'" ' + file + '.launch ' + file + '_GMF.canard',
 		function (error, stdout, stderr){
 			console.log('Output -> ' + stdout);
 			if(error !== null){
